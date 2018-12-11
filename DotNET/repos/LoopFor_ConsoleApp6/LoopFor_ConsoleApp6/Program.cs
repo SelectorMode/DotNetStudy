24e0f55ad7b7d8ca0a277ddec4972763bfad9517
Sergey Khomenko <sergey.khomenko@dataart.com>
2018-12-11 12:56:55 +0200

    Adding LoopFor file

---
+++ DotNET/repos/LoopFor_ConsoleApp6/LoopFor_ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LoopFor_ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<10; i++)
+            {                
+                if (i == 2) // skip when i=2(3)
+                {
+                    continue;
+                }
+                if (i == 5) //stop when i=4(5)
+                {
+                    break;
+                }
+                Console.WriteLine("message " + i);
+            }
+            Console.ReadKey();
+        }        
+    }
+}
