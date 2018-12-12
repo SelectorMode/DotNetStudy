66a8364de9a7eca449c932e12db7993159e4150a
SelectorMode <45737486+SelectorMode@users.noreply.github.com>
2018-12-12 23:13:13 +0200

    add GetSet file

---
+++ DotNET/repos/GetSet_ConsoleApp10/GetSet_ConsoleApp10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GetSet_ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+
+            human.age = 50;
+            human.name = "Pedro";
+
+            ShowGetMEthot(human); // transfer data to ShowMethod
+
+        }
+
+        static void ShowGetMEthot(Human human)
+        {
+            int age = human.age;
+            string name = human.name;
+            Console.WriteLine(name + " is " + age);
+            Console.ReadKey();
+        }
+    }
+
+    class Human
+    {
+        public int age;
+        public string name;
+
+        private int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+        private string Name
+        {
+            get
+            {
+                return name;
+
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
---
+++ DotNET/repos/MethodMax_ConsoleApp9/MethodMax_ConsoleApp9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodMax_ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int res;
+            NumberManipulator numberManipulator = new NumberManipulator();
+
+            res = numberManipulator.FindMax(30, 20);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+class NumberManipulator
+    {
+        public int FindMax(int first, int second)
+
+        {
+            int result;
+            if (first > second)
+            {
+                result = first;
+            }
+            else
+            {
+                result = second;
+            }
+            return result;
+
+        }
+
+    }
+}
