58339c8db916136071478521f19b9768820cdfb5
SelectorMode <45737486+SelectorMode@users.noreply.github.com>
2018-12-16 22:39:20 +0200

    adding 11,12,13 files

---
+++ DotNET/repos/PolyMorph_ConsoleApp13/PolyMorph_ConsoleApp13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PolyMorph_ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[2];
+
+            Dog dog = new Dog();
+            dog.Name = "Sharik";
+
+            Boozer boozer = new Boozer();
+            boozer.Name = "Zloebuch";
+
+            animals[0] = dog;
+            animals[1] = boozer;
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(animals[i] + " says: ");
+                animals[i].GetRoar();
+            }
+
+            Test test = new Test();
+            test.ShowContent();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public virtual void GetRoar()
+        {
+            Console.WriteLine("Abstrakt Roar");
+        }
+    }
+
+    class Dog : Animal
+    {// ovveride of the method
+        public override void GetRoar()
+        {
+            Console.WriteLine("Bow-wow");
+            base.GetRoar(); // refers to base class method: "Abstrakt Roar"
+        }
+    }
+
+    class Boozer : Animal
+    {// ovveride of the method
+        public override void GetRoar()
+        {
+            Console.WriteLine("What have you done since yesterday?");
+        }
+    }
+    class Test
+        
+    {
+        int a = 2;
+
+        public void ShowContent()
+        {
+            int a = 3;
+            Console.WriteLine(a);
+            Console.WriteLine(this.a);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ DotNET/repos/ThisBase_ConsoleApp14/ThisBase_ConsoleApp14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ThisBase_ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Id = 2;
+
+            ChangeId(book); // call method with book.Id = 1; inside.
+
+            Console.WriteLine(book.Id);
+            Console.ReadKey();
+
+
+            //ThisBase thisbase = new ThisBase();
+
+        }
+
+        static void ChangeId(Book book)
+        {
+            book.Id = 1;
+        }
+    }
+
+    class Book
+    {
+        public int Id { get; set; }
+    }
+    
+}
+
