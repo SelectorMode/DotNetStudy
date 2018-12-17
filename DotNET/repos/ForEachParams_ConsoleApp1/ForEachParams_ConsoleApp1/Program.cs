3af0c013ce028c63a5cde727bafed19b267c2c0c
SelectorMode <45737486+SelectorMode@users.noreply.github.com>
2018-12-17 23:25:22 +0200

    adding ForEachParams_file

---
+++ DotNET/repos/ForEachParams_ConsoleApp1/ForEachParams_ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForEachParams_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(1, 2, 3, 4, 5));
+            Console.ReadKey();
+        }
+
+        static int GetSum(params int[] arrayNumbers)
+        {
+            int sum = 0;
+            foreach (var number in arrayNumbers)
+            {
+                sum = sum + number;
+            }
+            return sum;
+        }
+
+    }
+
+
+}
