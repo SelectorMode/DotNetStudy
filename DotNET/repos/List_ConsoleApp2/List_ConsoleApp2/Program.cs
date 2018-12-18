85ce401ec401326af4e0b72d59edb60c29cdc094
Sergey Khomenko <sergey.khomenko@dataart.com>
2018-12-18 20:17:30 +0200

    string file

---
+++ DotNET/repos/List_ConsoleApp2/List_ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // необобщенная коллекция ArrayList
+            ArrayList objectList = new ArrayList() { 1, 2, "string", 'c', 2.0f };
+
+            object obj = 45.8;
+
+            objectList.Add(obj);
+            objectList.Add("string2");
+            objectList.RemoveAt(0); // удаление первого элемента
+            foreach (object o in objectList)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("Общее число элементов коллекции: {0}", objectList.Count);
+
+
+            // обобщенная коллекция List
+            List<string> countries = new List<string>() { "Россия", "США", "Великобритания", "Китай" };
+            countries.Add("Франция");
+            countries.RemoveAt(1); // удаление второго элемента
+            foreach (string s in countries)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNET/repos/String_ConsoleApp1/String_ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace String_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sampleStr1 = "someString";
+            string sampleStr2 = "somestring";
+            string sampleStr3 = "";
+            string sampleStr4 = string.Empty;
+            string fullName = "Khomenko,Sergey";
+
+
+            if (sampleStr1.Equals(sampleStr2, StringComparison.CurrentCultureIgnoreCase))
+            {                
+                for (int i = 0; i < sampleStr1.Length && i < sampleStr2.Length; i++)
+                {
+                    
+                    Console.WriteLine(i + " is " + sampleStr1[i] + " and " + sampleStr2[i]);
+                }
+                Console.WriteLine("Equals except cases");
+
+                string[] splitName = fullName.Split(',');
+                string joinedName = String.Join(";", splitName);
+
+                Console.WriteLine(joinedName);
+                Console.WriteLine(splitName[1] +" "+ splitName[0]);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
