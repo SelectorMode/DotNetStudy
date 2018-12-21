d500dbecd76623536eeb27ba1e0cbef9fb9f0cb1
Sergey Khomenko <sergey.khomenko@dataart.com>
2018-12-21 19:10:44 +0200

    pyramid

---
+++ DotNET/repos/EDU_Module2_Pyramid/EDU_Module2_Pyramid/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace EDU_Module2_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a simple program, that displays a pyramid in the console.
+            // Set number of rows as a variable:
+            // var rows = 7;
+            // Please, follow codestyles. 
+
+            // incrfeasing
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    //for (int j = columns - i; j < columns + 1; j++)
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // decriasing
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < columns - i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //decriasing more clear
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = i; j < rows; j++)
+            //    //for (int j = columns; j > i; j--)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //together
+            var rows = 7;
+
+            for (int i = 0; i < rows; i++)
+            {
+
+                for (int j = i; j < rows; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j <= i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
